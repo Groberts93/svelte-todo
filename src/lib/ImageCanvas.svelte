0027686cb10c158864d8bbc023019a152091b1d8
Gillian Roberts <gillian.roberts@renitamedical.com>
2023-01-15 22:27:19 +0000

    started image resizer component

--- src/lib/ImageCanvas.svelte
+++ src/lib/ImageCanvas.svelte
@@ -1,28 +1,75 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { claim_text } from "svelte/internal";
+
     export let src: string | null;
 
-    let image: HTMLImageElement;
     let canvas: HTMLCanvasElement;
-    
-    let canvas_width = 0;
-    let canvas_height = 0;
+    let ctx: CanvasRenderingContext2D;
+    const image: HTMLImageElement = new Image();
 
-    const drawImage = () => {
+    let canvas_width = 100;
+    let canvas_height = 100;
+    let scale = 0.5;
 
-        const {width, height} = image;
-        let downsizeRatio = 3;
+    let tx = 0;
+    let ty = 0;
+    /**
+     * A component allowing the user to rescale an image, select an area to be masked for use as an avatar, and save the resulting avatar.
+     */
+    image.src = src;
+    image.addEventListener("load", () => {
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, scale * image.width, scale * image.height);
+    });
 
+    const drawImage = (event: WheelEvent | MouseEvent) => {
+        if (ctx) {
+            if (event instanceof WheelEvent) {
+                scale = scale + event.deltaY / 1000;
+            } else if (event instanceof MouseEvent) {
+                tx = event.offsetX;
+                ty = event.offsetY;
+            }
 
-        canvas_width = width / downsizeRatio;
-        canvas_height = height / downsizeRatio;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(image, 0, 0, 300, 200);
-    }
-
+            ctx.drawImage(
+                image,
+                tx,
+                ty,
+                scale * image.width,
+                scale * image.height
+            );
+        }
+    };
 </script>
 
-<img on:load={drawImage} bind:this={image} {src} alt="nasdfasf" />
-<canvas id="image_resizer" bind:this={canvas}  width={300} height={200} />
+<!-- <img on:load={drawImage} bind:this={image} {src} alt="nasdfasf" /> -->
+<div id="container">
+    <canvas
+        id="image_resizer"
+        on:pointermove={drawImage}
+        on:wheel|preventDefault|nonpassive={drawImage}
+        bind:this={canvas}
+        width={250}
+        height={250}
+    />
+
+    <!-- <label for="scale">Scale: {scale}</label> -->
+    <!-- <input
+        on:change={changeScale}
+        bind:value={scale}
+        id="scale"
+        type="range"
+        min="0.25"
+        max="2.0"
+        step="0.1"
+    /> -->
+</div>
+
+<style>
+    #container {
+        display: grid;
+    }
+</style>
